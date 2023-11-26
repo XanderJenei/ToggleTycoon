6d6c731fa4163536dc0be40b3694bfe3f48c3011
Vincent Jenei <xanderjenei@icloud.com>
2023-11-26 13:44:44 -0800

    Initial Commit

---
+++ SliderSimulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SliderSimulator
+//
+//  Created by Vincent Jenei on 11/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SliderSimulator/SliderSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSimulatorApp.swift
+//  SliderSimulator
+//
+//  Created by Vincent Jenei on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SliderSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
