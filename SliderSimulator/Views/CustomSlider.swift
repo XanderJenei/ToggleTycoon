a8bcebf2fc0becc79205d355d109fcbdfba4e867
Vincent Jenei <xanderjenei@icloud.com>
2023-11-27 07:44:12 -0800

    Lots of work on the custom sliders, made an extra 2 custom and created animations, up to date as of day 1.

---
+++ SliderSimulator/Views/CustomSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSlider.swift
+//  SliderSimulator
+//
+//  Created by Vincent Jenei on 11/26/23.
+//
+
+import SwiftUI
+
+struct CustomSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSlider()
+}
